6ea6483ece44a3688f054198dbc31799a0d72223
str8uplinux <dana@str8uplinux.com>
2013-11-06 09:06:15 -0600

    Removed unneeded scripts folder

--- scripts/roles.php
+++
@@ -1,4 +0,0 @@
-<?php
-$user = strtolower(Getusername());
-$managers = array('alucht','dserafini','dsmith','hhoovestol','jharvey','jhoovestol','kwarden','lstarner','mcoates','sfigueroa','tcrimmins');
-?>
